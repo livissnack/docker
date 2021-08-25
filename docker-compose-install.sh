93d5c4c7db62bda02568d41ec0dc8a7fff53dc4f
bruce <=>
2021-08-25 18:02:08 +0800

    修改docker构建文件

---
+++ docker-compose-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+DOCKER_COMPOSE_VERSION=${1:-'1.29.2'}
+
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+docker-compose --version
